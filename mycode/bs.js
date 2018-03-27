c90e9c312494e525a791ea31e39e147f95032cf3
sameer <cu.16bcs1598@gmail.com>
2018-03-27 11:31:50 +0530

    refactor and reusability
    lect 017

--- mycode/bs.js
+++ mycode/bs.js
@@ -2,24 +2,64 @@ console.log('starting bs.js');
 
 const fs=require('fs');
 
+//fetching notes
+var fetchNote =() =>{
+  try {
+    var notesString=fs.readFileSync('yup.json');
+    console.log(JSON.parse(notesString));
+    return JSON.parse(notesString);
+} catch (e) {
+  return [];
+  }
+};
+
+
+//save note
+var saveNote =(notes) =>{
+fs.writeFileSync('yup.json',JSON.stringify(notes));
+
+}
+
+//main function
+
 var addNotes = (title,body) => {
-  var notes=[];
+  var notes=fetchNote();
   var note={
     title,
     body
   };
-  try {
-    var a=fs.readFileSync('yup.json');
-    notes=JSON.parse(a);
 
-  } catch (e) {
-    
-  }
+  //filter duplicate note
+var duplicatenote=notes.filter((note)=>{
+  return note.title===title;
+});
 
+
+if(duplicatenote.length===0){
   notes.push(note);
-  fs.writeFileSync('yup.json',JSON.stringify(notes));
+  saveNote(notes);
+  return note;
+}
+};
+
+
+
+
+var getAll=() =>{
+  console.log('getting all notes');
+};
+
+var getNote=(title) =>{
+  console.log('getting note',title);
+};
+var removeNote=(title)=>{
+  console.log('removing note',title);
 };
 
 module.exports={
-  addNotes
+  addNotes,
+  getAll,
+  getNote,
+  removeNote
+
 };
